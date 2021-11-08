9752afd47ec48e9a09c105aa9aa99c381e7f72e6
Yuri Migushin <imigushin@gmail.com>
2021-11-09 02:27:12 +0300

    Added main text and cor.styles

--- App.js
+++ App.js
@@ -5,13 +5,23 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-
+      <Text style={styles.text}>Список дел</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  main: {
+    paddingTop: 50,
+    height: 100,
+    backgroundColor: 'silver'
+  },
+  text: {
+    fontSize: 18,
+    color: 'black',
+    fontWeight: 'bold'
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
