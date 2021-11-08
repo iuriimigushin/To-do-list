26d130a7069e8d09de478459be3e4bcc02e0902a
Yuri Migushin <imigushin@gmail.com>
2021-11-09 02:29:35 +0300

    Added Header import to Header

---
+++ components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Header.js;
+
+export default function App() {
+    return (
+        <View>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
