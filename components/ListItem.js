66e030a5b557de4f2387015b1997dce5c01cc3ef
Yuri Migushin <imigushin@gmail.com>
2021-11-09 22:04:43 +0300

    Added styles for text in ListItem.js

--- components/ListItem.js
+++ components/ListItem.js
@@ -12,5 +12,15 @@ export default function ListItem({ el }) {
 }
 
 const styles = StyleSheet.create({
+    text: {
+        padding: 20,
+        textAlign: 'center',
+        borderRadius: 5,
+        backgroundColor: '#fafafa',
+        borderWidth: 1,
+        marginTop: 20,
+        width: '60%',
+        marginLeft: '20%'
+    }
 
 });
