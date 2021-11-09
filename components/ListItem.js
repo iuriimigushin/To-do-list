4c233706da2ad60b7c1684c24080ed6277a70228
Yuri Migushin <imigushin@gmail.com>
2021-11-09 21:59:10 +0300

    Fixed function name in ListItem

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, TouchableHighlight } from 'react-native';
 
-export default function Header({ el }) {
+export default function ListItem({ el }) {
     return (
         <TouchableHighlight>
             <Text style={styles.text}>
