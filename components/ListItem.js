bcdec7d40ed818aa0d462266bc1c4955ed534d48
Yuri Migushin <imigushin@gmail.com>
2021-11-09 21:55:16 +0300

    Added hedaer for file ListItem

---
+++ components/ListItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableHighlight } from 'react-native';
+import Header from './components/Header';
+
+export default function App() {
+
+
+    return (
+        <View>
+            <Header />
+            <View>
+
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+
+});
