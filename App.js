745c34f201cbbfde4dcac29f2b2f39551002ea54
Yuri Migushin <imigushin@gmail.com>
2021-11-09 21:44:12 +0300

    Added list of tasks: listOfItems, setListOfItems

--- App.js
+++ App.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 
 export default function App() {
+  const [listOfItems, setListOfItems] = useState([
+    { text: 'Дело 1', index: 1 },
+    { text: 'Дело 2', index: 1 },
+    { text: 'Дело 3', index: 1 },
+    { text: 'Дело 4', index: 1 },
+  ])
+
   return (
     <View>
       <Header />
