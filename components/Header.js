7093d1895cbe69a7322633e6dfed12f7a68d655e
Yuri Migushin <imigushin@gmail.com>
2021-11-09 21:53:17 +0300

    Fixed function name

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function Header() {
     return (
         <View style={styles.container}>
             <Text style={styles.text}>Список дел</Text>
